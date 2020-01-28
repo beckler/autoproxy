63e8ce9fd151a2faee6a7ddac08b849e8dc890d4
Michael Beckler <mcbeckler@gmail.com>
2020-01-27 23:52:31 -0500

    moved to better logging

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,87 @@
+use log::{debug, error};
 use structopt::StructOpt;
 use structopt::clap::Shell;
+use structopt::clap::AppSettings;
 
 mod model;
 
+// cli structs
+#[derive(StructOpt, Debug)]
+#[structopt(about, global_settings = &[AppSettings::DisableVersion])]
+struct LaunchArgs{
+    /// Silence all output
+    #[structopt(short = "q", long = "quiet")]
+    quiet: bool,
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: usize,
+    // subcommand
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// Prints the current status
+    #[structopt(name = "status")]
+    Status {},
+    /// Enables the autoproxy
+    #[structopt(name = "enable")]
+    Enable {},
+    /// Disables the autoproxy
+    #[structopt(name = "disable")]
+    Disable {},
+    /// List all proxy configurations and their status
+    #[structopt(name = "list")]
+    List {},
+    /// Adds a new proxy configuration
+    #[structopt(name = "add")]
+    Add(model::Proxy),
+    /// Removes an existing proxy configuration
+    #[structopt(name = "remove")]
+    Remove {
+        /// Proxy configuration alias
+        name: String,
+    },
+}
+
 fn main() {
     // generate bash completions
-    model::LaunchArgs::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::Bash, "target");
+    LaunchArgs::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::Bash, "target");
 
     // parse command line args (if passed in)
-    let opt = model::LaunchArgs::from_args();
+    let opt = LaunchArgs::from_args();
 
-    // set verbosity
-    let verbosity: u8 = opt.verbose;
+    // setup logging
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(opt.quiet)
+        .verbosity(opt.verbose)
+        .timestamp(stderrlog::Timestamp::Off)
+        .init()
+        .unwrap();
 
     // parse config file
+    debug!("retrieving and parsing config file");
     let mut cfg: model::Config = match confy::load(model::APP_NAME) {
         Ok(data) => data,
         Err(err) => {
-            println!("Error with config file: {:?}", err);
+            error!("Unable to load config file: {:?}", err);
             std::process::exit(1);
         },
     };
+    debug!("successfully loaded config file");
 
     // parse cmd line option and execute
     match opt.cmd {
         Some(cmd) => match cmd {
-            model::Command::Status{} => cfg.status(verbosity),
-            model::Command::Enable{} => cfg.enable_proxy(verbosity),
-            model::Command::Disable{} => cfg.disable_proxy(verbosity),
-            model::Command::List{} => cfg.list_proxies(verbosity),
-            model::Command::Remove{ name } => cfg.remove_proxy(verbosity, name),
-            model::Command::Add(proxy) => cfg.add_proxy(verbosity, proxy),
+            Command::Status{} => cfg.status(),
+            Command::Enable{} => cfg.enable_proxy(),
+            Command::Disable{} => cfg.disable_proxy(),
+            Command::List{} => cfg.list_proxies(),
+            Command::Remove{ name } => cfg.remove_proxy(name),
+            Command::Add(proxy) => cfg.add_proxy(proxy),
         },
-        None => cfg.determine_proxy(verbosity)
+        None => cfg.determine_proxy()
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -1,54 +1,17 @@
+use std::env;
 use std::net::TcpStream;
+use log::{info, debug, warn, error};
+use console::style;
 use structopt::StructOpt;
-use structopt::clap::AppSettings;
 use serde_derive::{Deserialize, Serialize};
 
 pub const APP_NAME: &'static str = "autoproxy";
 
-// cli structs
-#[derive(StructOpt, Debug)]
-#[structopt(about, global_settings = &[AppSettings::DisableVersion])]
-pub struct LaunchArgs{
-    // /// Activate debug mode
-    // #[structopt(short, long)]
-    // debug: bool,
-    // The number of occurrences of the `v/verbose` flag
-    /// Verbose mode (-v, -vv, -vvv, etc.)
-    #[structopt(short, long, parse(from_occurrences))]
-    pub verbose: u8,
-    // subcommand
-    #[structopt(subcommand)]
-    pub cmd: Option<Command>,
-}
-
-#[derive(StructOpt, Debug)]
-pub enum Command {
-    #[structopt(name = "status")]
-    Status {},
-    /// Enables the autoproxy
-    #[structopt(name = "enable")]
-    Enable {},
-    /// Disables the autoproxy
-    #[structopt(name = "disable")]
-    Disable {},
-    /// List all proxy configurations and their status
-    #[structopt(name = "list")]
-    List {},
-    /// Adds a new proxy configuration
-    #[structopt(name = "add")]
-    Add(Proxy),
-    /// Removes an existing proxy configuration
-    #[structopt(name = "remove")]
-    Remove {
-        /// Proxy configuration alias
-        name: String,
-    },
-}
 
 // config structs
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    enabled: Option<bool>,
+    enabled: bool,
     proxy: Option<Vec<Proxy>>,
 }
 
@@ -56,6 +19,10 @@ pub struct Config {
 pub struct Proxy {
     /// Alias for this proxy configuration
     pub name: String,
+    /// Domain:Port to test proxy connections.
+    /// This tests the connection by creating a TCP connection.
+    #[structopt(short, long, default_value="docs.tnybit.com:80")]
+    pub test_url: String,
     /// HTTP proxy endpoint
     #[structopt(long)]
     pub http: Option<String>,
@@ -71,34 +38,50 @@ pub struct Proxy {
 impl Default for Config {
     fn default() -> Self {
         Config {
-            enabled: Some(false),
+            enabled: false,
             proxy: None,
         }
     }
 }
 
 impl Config {
-    pub fn status (&self, _verbosity: u8) {
-        //todo: print status (enabled, disabled)
-        //todo: let user know if proxy varibles are currently set
-        //todo?: show status of proxy connections? - might be better in it's own subcommand.
+    // TODO: needs tests
+    pub fn status (&self) {
+        // enabled/disabled status
+        println!("autoproxy is currently {}", if self.enabled { 
+            style("enabled").green() 
+        } else { 
+            style("disabled").red() 
+        });
+        // proxy status
+        print_status("https_proxy");
+        print_status("HTTPS_PROXY");
+        print_status("http_proxy");
+        print_status("HTTP_PROXY");
+        print_status("no_proxy");
+        print_status("NO_PROXY");
     }
 
-    pub fn enable_proxy(&mut self, verbosity: u8) {
-        self.enabled = Some(true);
-        self.update_config(verbosity);
+    // TODO: needs tests
+    pub fn enable_proxy(&mut self) {
+        debug!("autoproxy is being enabled");
+        self.enabled = true;
+        self.update_config();
     }
 
-    pub fn disable_proxy(&mut self, verbosity: u8) {
-        self.enabled = Some(false);
-        self.update_config(verbosity);
+    // TODO: needs tests
+    pub fn disable_proxy(&mut self) {
+        debug!("autoproxy is being disabled");
+        self.enabled = false;
+        self.update_config();
     }
 
-    pub fn remove_proxy(&mut self, verbosity: u8, name: String) {
+    // TODO: needs tests
+    pub fn remove_proxy(&mut self, name: String) {
         match &mut self.proxy {
             // if there are no configs availabled, exit early.
             None => {
-                println!("there are no proxy configurations available to remove");
+                warn!("there are no proxy configurations available to remove");
                 std::process::exit(1);
             },
             // if we have proxy data available...
@@ -107,50 +90,51 @@ impl Config {
                 match data.iter().position(|x| x.name == name) {
                     None => {
                         // if we're unable to find the config with the specific name, exit.
-                        println!("unable to find a proxy configuration with the name: {:?}", name);
+                        warn!("unable to find a proxy configuration with the name: {:?}", name);
                         std::process::exit(1);
                     },
                     Some(idx) => {
                         // remove the entry, and save the config.
+                        debug!("removing entry from config file");
                         data.remove(idx);
-                        self.update_config(verbosity);
+                        self.update_config();
                     }
                 }
             }
         };
+        println!("removed proxy")
     }
 
-    pub fn add_proxy(&mut self, verbosity: u8, proxy: Proxy) {
+    // TODO: needs tests
+    pub fn add_proxy(&mut self, proxy: Proxy) {
         // add a new proxy
         match &mut self.proxy {
             None => {
                 // create a new list if none exists
+                debug!("config file is empty - added data to new config");
                 let mut new_list: Vec<Proxy> = Vec::<Proxy>::new();
                 new_list.push(proxy);
 
                 self.proxy = Some(new_list);
             },
             Some(data) => {
+                debug!("adding proxy to config file");
                 data.push(proxy);
             }
         }
 
         // update config file
-        self.update_config(verbosity);
-        if verbosity > 0 {
-            println!("proxy added to configuration");
-        }
+        self.update_config();
+        println!("added proxy");
     }
 
-    pub fn list_proxies(&self, verbosity: u8) {
-        if verbosity > 1 {
-            println!("listing available proxies")
-        }
-
+    // TODO: needs tests
+    pub fn list_proxies(&self) {
+        debug!("listing all available proxies");
         let proxies: Vec<Proxy> = match &self.proxy {
             Some(data) => data.to_vec(),
             None => {
-                println!("there are no proxy configurations available");
+                warn!("there are no proxy configurations available");
                 std::process::exit(0);
             },
         };
@@ -175,27 +159,36 @@ impl Config {
         }
     }
 
-    pub fn determine_proxy(&self, verbosity: u8) {
+    // TODO: needs tests
+    pub fn determine_proxy(&self) {
         if let Ok(_) = TcpStream::connect("google.com:443") {
-            if verbosity > 0 {
-                println!("Connected to the server!");
-            }
+            info!("connected to the server");
         } else {
-            println!("Couldn't connect to server...");
+            info!("unable to connect to server");
         }
         std::process::exit(0)   
     }
+
+    fn update_environment(&self) {
+        
+    }
     
-    fn update_config(&self, verbosity: u8) {
-        if verbosity > 1 {
-            println!("writing config to filesystem");
-        }
+    // TODO: needs tests
+    fn update_config(&self) {
+        debug!("writing or updating config to filesystem");
         match confy::store(APP_NAME, self) {
             Ok(_) => (),
             Err(err) => {
-                println!("Error writing to config file: {:?}", err);
+                error!("unable to write to config file: {:?}", err);
                 std::process::exit(2)
             },
         };
+    }
+}
+
+fn print_status(key: &str) {
+    match env::var_os(key) {
+        Some(val) => println!(" - {} is {} to: {:?}", key, style("currently set").green(), val),
+        None => println!(" - {} is {}", key, style("not currently set").cyan()),
     }
 }
